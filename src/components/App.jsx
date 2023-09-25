9ca1f97b355a28c08f5ebb4be40cdfd3f470fde1
Yulia503 <125694915+Yulia503@users.noreply.github.com>
2023-09-26 00:19:19 +0300

    updape-04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -9,33 +9,36 @@ import { Container } from './Container';
 import { StyledNotification } from './Notification.styled';
 
 const LS_KEY = 'contacts';
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
 
-    componentDidMount() {
-    const contacts = localStorage.getItem(LS_KEY);
-    const actualContacts = JSON.parse(contacts);
+//*------------------- Отримуємо контакти з Local St-----------
 
-    if (actualContacts) {
-      this.setState({ contacts: actualContacts });
-    }
+const getActualContacts = () => {
+  const savedPreviousContacts = localStorage.getItem(LS_KEY);
+
+  if (savedPreviousContacts) {
+    return JSON.parse(savedPreviousContacts);
   }
+  
+  return [];
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    const currentContacts = this.state.contacts;
-    const previousContacts = prevState.contacts;
-
-    if (currentContacts !== previousContacts) {
-      localStorage.setItem(LS_KEY, JSON.stringify(currentContacts));
-    }
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState(getActualContacts);
+  const [filter, setFilter] = useState('');
 
 
-  addContact = newContact => {
-    const isContactInList = this.state.contacts.some(
+
+  //*-------------- Нам потрібно записати наші контакти в LS-----------------
+
+
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+
+
+  const addNewContact = newContact => {
+    const isContactInList = contacts.some(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
 
@@ -44,61 +47,68 @@ export class App extends Component {
       return;
     }
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: nanoid(), ...newContact }],
-    }));
+    setContacts(prevContacts => [
+      ...prevContacts,
+      { id: nanoid(), ...newContact },
+    ]);
   };
 
-  filterContactsByName = newContactName => {
-    this.setState({
-      filter: newContactName,
-    });
-  };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
 
-  render() {
-    const { contacts, filter } = this.state;
-
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+//*--------------------Delete----------------
+  
+  
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
     );
+  };
 
-    return (
-      <>
-        <StyledSection>
-          <Container>
-            <SectionTitle>Phonebook</SectionTitle>
-            <ContactForm onAdd={this.addContact} />
-          </Container>
-        </StyledSection>
 
-        <StyledSection>
-          <Container>
-            <ContactsTitle>Contacts</ContactsTitle>
-            <ContactFilter
-              filter={filter}
-              onContactFilter={this.filterContactsByName}
+  const filterContactsByName = newContactName => {
+    setFilter(newContactName);
+  };
+
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+
+
+
+  return (
+    <>
+      <StyledSection>
+        <Container>
+          <SectionTitle>Phonebook</SectionTitle>
+          <ContactForm onAdd={addNewContact} />
+        </Container>
+      </StyledSection>
+
+      <StyledSection>
+        <Container>
+          <ContactsTitle>Contacts</ContactsTitle>
+          <ContactFilter
+            filter={filter}
+            onContactFilter={filterContactsByName}
+          />
+          {contacts.length > 0 && filteredContacts.length > 0 ? (
+            <ContactList
+              contacts={filteredContacts}
+              onDeleteContactItem={deleteContact}
             />
-            {contacts.length > 0 ? (
-              <ContactList
-                contacts={filteredContacts}
-                onDeleteContactItem={this.deleteContact}
-              />
-            ) : (
-              <StyledNotification>
+          ) : (
+            <StyledNotification>
                 You don't have a friend with this name, Please add some contact to the
                 phonebook!
-              </StyledNotification>
-            )}
-          </Container>
-        </StyledSection>
-        <GlobalStyle />
-      </>
-    );
-  }
-}
+            </StyledNotification>
+          )}
+        </Container>
+      </StyledSection>
+      <GlobalStyle />
+    </>
+  );
+};
+
+
